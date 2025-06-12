f1305e2c266c72e33c0f3b412c26cd2420c2ee0b
matthew <matthewyt765@gmail.com>
2025-06-12 17:49:41 +0800

    add prototype inheritance lessons

--- advanceJavascript/prototype.js
+++ advanceJavascript/prototype.js
@@ -6,7 +6,7 @@ function Person(first, last) {
 const user1 = new Person("Matthew David", "Fernandez");
 const user2 = new Person("Chariss", "Salvador");
 
-// INSTEAD OF THIS TYPE OF FUNCTION. WE CAN USE prototype! to available to all!
+// INSTEAD OF THIS TYPE OF FUNCTION. WE CAN USE prototype! to available to all like creating a method!
 // user1.getFullName = function () {
 //   return this.first + ' ' + this.last;
 // } 
@@ -20,3 +20,19 @@ console.log(user1.getFullName());
 console.log(user2.getFullName());
 
 // prototype inheritance
+
+function Friend(first, last) {
+  Person.call(this, first, last);
+  this.isFriend = true;
+}
+
+Friend.prototype = Object.create(Person.prototype)
+
+Friend.prototype.goodFriend = function () {
+  console.log(`${this.first} is a good friend!`);
+}
+const dave = new Friend("Dave", "Orbiada");
+
+
+dave.goodFriend();
+console.log(dave.getFullName());
