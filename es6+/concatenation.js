0e49a16c3f71105b959402f8ab5c62d74cb47251
matthew <matthewyt765@gmail.com>
2025-06-13 16:47:39 +0800

    add concatenation lesson

---
+++ es6+/concatenation.js
@@ -0,0 +1,11 @@
+// old javascript
+
+var name1 = "matthew";
+
+console.log("Hi! Welcome to our app " + name1); // if you used vs code the name variable have a strikethrought why? because it will be deprecated soon
+
+
+// es6+ 
+const name2 = "chariss";
+const age = "20" ;
+console.log(`Hi! Welcome to our app ${name2} and you are ${age}yrs old!`);
