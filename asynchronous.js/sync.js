8dc9c96dd304c55ce5699a4d2068b515ec0e83bd
matthew <matthewyt765@gmail.com>
2025-06-17 09:41:39 +0800

    add synchronous js lesson

---
+++ asynchronous.js/sync.js
@@ -0,0 +1,8 @@
+console.log("A STEP BY STEP CREATING A SCRAMBLE NA ITLOG!");
+console.log("step 1: batiin ang itlog.(HI itlog! joke) break the egg(alam niyo nayon)");
+console.log("step 2: lagyan ng asin at magic sarap kung hindi ka expose sa msg lately");
+console.log("step 3: haluin haluin");
+console.log("step 4: i prito na and pagkatapos ng 5-8minutes");
+console.log("step 5: i lagay na sa plato and shalaa kainan naa!");
+
+// this is an example of synchronous js its step by step print my clg
