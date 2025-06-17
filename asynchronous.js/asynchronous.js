503ceff30ebabde2078beba05fb19e6d16ba6726
matthew <matthewyt765@gmail.com>
2025-06-17 10:15:07 +0800

    add lesson asynchronous

---
+++ asynchronous.js/asynchronous.js
@@ -0,0 +1,25 @@
+console.log("A STEP BY STEP CREATING A SCRAMBLE NA ITLOG!");
+
+
+
+
+
+setTimeout(() => {
+  console.log("step 1: batiin ang itlog.(HI itlog! joke) break the egg(alam niyo nayon)");
+}, 5000);
+
+setTimeout(() => {
+  console.log("step 2: lagyan ng asin at magic sarap kung hindi ka expose sa msg lately");
+}, 3000);
+
+setTimeout(() => {
+  console.log("step 3: haluin haluin");
+}, 2000);
+
+setTimeout(() => {
+  console.log("step 4: i prito na and pagkatapos ng 5-8minutes");
+}, 4000);
+
+setTimeout(() => {
+  console.log("step 5: i lagay na sa plato and shalaa kainan naa!");
+}, 6000);
