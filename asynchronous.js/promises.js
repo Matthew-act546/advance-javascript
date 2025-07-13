d4963ecab813db6895e7a7a2ff80f0ac3941a82a
matthew <matthewyt765@gmail.com>
2025-07-13 14:35:47 +0800

    add promise.all function

--- asynchronous.js/promises.js
+++ asynchronous.js/promises.js
@@ -9,6 +9,7 @@ const promise1 = new Promise((resolve, reject) => {
     reject("Something went wrong sadly...")
   }
 })
+
 const promise2 = new Promise((resolve, reject) => {
   const randomNum = Math.floor(Math.random() * 10);
   setTimeout(() => {
@@ -24,7 +25,25 @@ promise2
   .then((result) => {
     console.log(result) 
     return promise1
-  }).then((result) => {
+  })
+  .then((result) => {
     console.log(result);
-  }).catch(error => console.log(error));
+  })
+  .catch(error => console.log(error));
 
+const promiseOne = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("resolved")
+  }, 2000)
+})
+
+const promiseTwo = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("resolved promise two")
+    reject("rejected promise two")
+  }, 2000)
+})
+
+Promise.all([promiseOne, promiseTwo])
+  .then((data) => console.log(data[0], data[1]))
+  .catch((error) => console.log(error))
